5b640f16fb045a5865e717bc6494d9e8b5042e83
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-24 14:18:53 +0200

    Add comments to reducer.js

--- reducer.js
+++ reducer.js
@@ -1,15 +1,20 @@
-import { initialState } from "./initialState.js";
-import { ADD, SUBTRACT, RESET } from "./action.js";
+import { initialState } from "./initialState.js"; // Imports initial state for the counter
+import { ADD, SUBTRACT, RESET } from "./action.js"; // Imports action types
 
+// Reducer function to manage state updates for the counter
 export function reducer(state = initialState, action) {
   switch (action.type) {
     case ADD:
+      // Increment count by 1 when ADD action is dispatched
       return { count: state.count + 1 };
     case SUBTRACT:
+      // Decrement count by 1 when SUBTRACT action is dispatched
       return { count: state.count - 1 };
     case RESET:
+      // Reset count to 0 when RESET action is dispatched
       return { count: 0 };
     default:
+      // Return current state if action type is not recognized
       return state;
   }
 }
