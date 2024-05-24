1c7caaf038b82526962c200220a90653e332d62e
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-24 15:27:20 +0200

    Implement a Global Store

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 import { reducer } from "./reducer.js";
 import { ADD, SUBTRACT, RESET } from "./action.js"; // Imports action types
 
+//Store implementation using the reducer
+function createStore(reducer) {
+  let currentState = reducer(undefined, {});
+  let listeners = [];
+  //Function to get current state and to dispatch actions then udate state
+  return {
+    getState: () => currentState,
+    dispatch: (action) => {
+      currentState = reducer(currentState, action); //Function to update state
+      listeners.forEach((listener) => listener()); //function to notify all listeners
+    },
+    //Function to subscribe to state changes, add listener to array and then remore listener from the array
+    subscribe: (listener) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter((l) => l !== listener);
+      };
+    },
+  };
+}
 
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// SCENARIO 1: Initial State Verification
+console.log(store.getState());
+
+// Scenario 2: Incrementing the Counter
+store.dispatch({ type: ADD });
+store.dispatch({ type: ADD });
+
+// Scenario 3: Decrementing the Counter
+store.dispatch({ type: SUBTRACT });
+
+// Scenario 4: Resetting the Counter
+store.dispatch({ type: RESET });
